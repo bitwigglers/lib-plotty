8b403a24e01c1238fcdebc6729a733f5abc341e6
Andre Haupt <andre@bitwigglers.org>
2009-08-10 17:04:14 +0200

    main: change example graph

--- main.cpp
+++ main.cpp
@@ -12,32 +12,25 @@ int main(int argc, char *argv[])
 	PlotSettings settings;
 	settings.minY = -2;
 	settings.maxY = 2;
-	settings.minX = - 10;
-	settings.maxX = 10;
+	settings.minX = -2 * M_PI;
+	settings.maxX = 2 * M_PI;
 	plotter.setPlotSettings(settings);
 
 	QVector<QPointF> v1, v2, v3, v4, v5;
 
-	double x = -10;
-	while (x <= 10) {
+	for (int i = -359; i < 360; i++) {
+		double x = i * M_PI / 180;
 		double y = sin(x);
 		v1.append(QPointF(x, y));
-
 		y += 1.0/3.0 * sin(3.0 * x);
 		v2.append(QPointF(x, y));
-
 		y += 1.0/5.0 * sin(5.0 * x);
 		v3.append(QPointF(x, y));
-
 		y += 1.0/7.0 * sin(7.0 * x);
 		v4.append(QPointF(x, y));
-
 		y += 1.0/9.0 * sin(9.0 * x);
 		v5.append(QPointF(x, y));
-
-		x += 0.1;
 	}
-
 
 	plotter.setCurveData(0, v1);
 	plotter.setCurveData(1, v2);
